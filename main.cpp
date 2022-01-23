4a72866cd7e541d4538942f58398bace3aa188a8
dakini <dakini.mallam-garba@insa-rennes.fr>
2022-01-23 19:55:18 +0100

    update Main

--- main.cpp
+++ main.cpp
@@ -7,115 +7,26 @@
 #include <string>
 #include <algorithm>
 
-#include "header/Coin.hpp"
-#include "header/Star.hpp"
-#include "header/Pixou.hpp"
-#include "header/Position.hpp"
+#include "header/Game.hpp"
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    srand(time(NULL));
-
-    int N = 100;
-    vector<Reward *> rewards;
-    int nb_rewards = 25;
-
-    for (int i = 0; i < nb_rewards; i++)
-    {
-        int pos = rand() % 100 + 1;
-
-        if (i < 20)
-        {
-            Coin *c = new Coin(pos);
-            rewards.push_back((Reward *)c);
-        }
-        else
-        {
-            Star *s = new Star(pos);
-            rewards.push_back(s);
-        }
-    }
-
-    Pixou *salim = new Pixou("Salim", 0);
-
-    int i = 0;
-    vector<Reward *> inWindow;
-    while (true)
-    {
-        if (i < 25)
-        {
-            inWindow.push_back(rewards[i]);
-        }
-
-        int rand_moves = rand() % 3 + (-1);
-        salim->movePixou(rand_moves);
-        cout << "Position of Pixou  x : " << salim->position().x() << endl;
-
-        for (auto rw : inWindow)
-        {
-            rw->dropReward();
-            int rw_points = rw->rewards();
-
-            cout << "Position of Reward"
-                 << " x : " << rw->position().x() << " y : " << rw->position().y() << endl;
-
-            if (rw->outOfWindow())
-            {
-                inWindow.erase(remove(inWindow.begin(), inWindow.end(), rw), inWindow.end());
-
-                salim->updatePoints(-rw_points);
-            }
-
-            if (rw->position() == salim->position())
-            {
-                salim->updatePoints(rw_points);
-                inWindow.erase(remove(inWindow.begin(), inWindow.end(), rw), inWindow.end());
-            }
-        }
-
-        cout << "Pixou : " << salim->name() << " nb Points : " << salim->points() << endl;
-
-        i++;
-        //sleep(2);
-
-        if (inWindow.empty())
-        {
-            cout << "GAME OVER "
-                 << " Final Score " << salim->points() << endl;
-            break;
-        }
-    }
-
-    return 0;
-}
-
-/**
-
-#include <SFML/Graphics.hpp>
-
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    srand(time(NULL));
+    //Init game
+    Game game;
 
-    while (window.isOpen())
+    while (game.running())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        //update
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        game.update();
+
+        //render
+
+        game.render();
     }
 
     return 0;
 }
-
-**/
